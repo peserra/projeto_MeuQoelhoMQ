fdce88a127760aa36cb6f4c26b39bcfcff9fb3ef
Elton Silva <elton.silva@nubank.com.br>
2024-08-10 20:25:43 -0300

    format code

--- scala/src/main/scala/grpc/Client.scala
+++ scala/src/main/scala/grpc/Client.scala
@@ -1,12 +1,13 @@
 package grpc
 
-import io.grpc.ManagedChannelBuilder
+import io.grpc.{ManagedChannel, ManagedChannelBuilder}
 import teste_grpc.simple.{ChannelType, CreateChannelRequest, ListChannelsRequest, MessageManagerGrpc}
 import scala.concurrent.ExecutionContext.Implicits.global
+import teste_grpc.simple.MessageManagerGrpc.MessageManagerStub
 
 object Client extends App {
-    val channel = ManagedChannelBuilder.forAddress("localhost", 12345).build()
-    val stub = MessageManagerGrpc.stub(channel)
+    val channel: ManagedChannel = ManagedChannelBuilder.forAddress("localhost", 12345).build()
+    val stub: MessageManagerStub = MessageManagerGrpc.stub(channel)
 
     val createChannelRequest: CreateChannelRequest = CreateChannelRequest("canal1", ChannelType.SIMPLE)
     val listChannelsRequest: ListChannelsRequest = ListChannelsRequest()
