3d42604a65705513bfd8a4a62a0509daf02283f9
Elton Silva <elton.silva@nubank.com.br>
2024-08-11 23:37:16 -0300

    improvements on client

--- scala/src/main/scala/grpc_client/Client.scala
+++ scala/src/main/scala/grpc_client/Client.scala
@@ -1,19 +1,25 @@
 package grpc_client
 
 import io.grpc.{ManagedChannel, ManagedChannelBuilder}
-import teste_grpc.simple.{ChannelType, CreateChannelRequest, CreateChannelResponse, ListChannelsRequest, MessageManagerGrpc}
+import teste_grpc.simple.{ChannelType, CreateChannelRequest, CreateChannelResponse, ListChannelsRequest, ListChannelsResponse, MessageManagerGrpc}
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 import teste_grpc.simple.MessageManagerGrpc.MessageManagerStub
 
 object Client extends App {
-    val channel: ManagedChannel = ManagedChannelBuilder.forAddress("localhost", 12345).build()
-    val stub: MessageManagerStub = MessageManagerGrpc.stub(channel)
+    val channel1: ManagedChannel = ManagedChannelBuilder.forAddress("127.0.0.1", 12345).build()
+    val stub: MessageManagerStub = MessageManagerGrpc.stub(channel1)
 
     val createChannelRequest: CreateChannelRequest = CreateChannelRequest("canal1", ChannelType.SIMPLE)
-    val listChannelsRequest: ListChannelsRequest = ListChannelsRequest()
-    val createChannelResponse: Future[CreateChannelResponse] = stub.createChannel(createChannelRequest)
-    println(s"channel created: ${createChannelResponse.toString}")
 
-    stub.listChannels(listChannelsRequest).foreach(println)
+    val listChannelsRequest: ListChannelsRequest = ListChannelsRequest()
+
+    val createChannelResponse: Future[CreateChannelResponse] = stub.createChannel(createChannelRequest)
+    
+    println("recebido do server " + createChannelResponse.toString)
+
+    val listChannelsResponse: Future[ListChannelsResponse] = stub.listChannels(listChannelsRequest)
+      
+      listChannelsResponse.foreach(println)
+    
   }
